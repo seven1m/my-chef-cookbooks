94cde1b1421e6043e9586e32e1c63cf78e66cc2d
Tim Morgan <tim@timmorgan.org>
2014-07-05 16:58:08 -0500

    Install Ruby 2.1.2

--- cookbooks/base/recipes/ruby.rb
+++ cookbooks/base/recipes/ruby.rb
@@ -1,7 +1,19 @@
-%w(ruby ruby-dev libxml2-dev libxslt-dev libreadline-ruby libruby libopenssl-ruby build-essential libmysql-ruby).each do |pkg|
+%w(libxml2-dev libxslt-dev build-essential).each do |pkg|
   package(pkg) do
     action :install
   end
+end
+
+bash 'install_ruby' do
+  creates '/usr/bin/ruby'
+  code <<-EOH
+    cd /tmp
+    curl -O http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz
+    tar xvzf ruby-2.1.2.tar.gz
+    cd ruby-2.1.2
+    ./configure
+    make install
+  EOH
 end
 
 cookbook_file('gemrc') do
